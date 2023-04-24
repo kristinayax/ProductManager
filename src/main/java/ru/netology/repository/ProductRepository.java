7c06dadad161148badf5a7f5036841c88e2f7c37
kristinayax <kristinayax@icloud.com>
2023-04-24 21:48:15 +0300

    Added new files

---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,41 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Product;
+
+
+public class ProductRepository {
+
+    private Product[] products = new Product[0];
+
+    public void save(Product product) {
+
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+
+    public Product[] findAll() {
+        return products;
+    }
+
+
+    public void removeById(int Id) {
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+
+            if (product.getId() != Id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+
+    }
+
+
+}
